9c99f975f36ef1607947fcf9f20f5a119dd1e116
parksg <park0110725@naver.com>
2024-05-22 09:33:29 +0900

    회원가입화면
    가입 버튼 클릭시
    post /api/member/signup
    전송

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -25,6 +25,15 @@ export function Navbar() {
       >
         글쓰기
       </Box>
+      <Box
+        onClick={() => navigate("/signup")}
+        cursor={"pointer"}
+        _hover={{
+          bgColor: "grey.200",
+        }}
+      >
+        회원가입
+      </Box>
     </Flex>
   );
 }
