19beb1ca2379840de24139520ea3e11644c8ab58
parksg <park0110725@naver.com>
2024-05-30 16:28:51 +0900

    댓글 지우고 목록 다시 로딩

--- src/component/comment/CommentComponent.jsx
+++ src/component/comment/CommentComponent.jsx
@@ -4,16 +4,20 @@ import { CommentWrite } from "./CommentWrite.jsx";
 import { useState } from "react";
 
 export function CommentComponent({ boardId }) {
-  const [isSending, setIsSending] = useState(false);
+  const [isProcessing, setIsProcessing] = useState(false);
 
   return (
     <Box>
       <CommentWrite
         boardId={boardId}
-        isSending={isSending}
-        setIsSending={setIsSending}
+        isProcessing={isProcessing}
+        setIsProcessing={setIsProcessing}
       />
-      <CommentList boardId={boardId} isSending={isSending} />
+      <CommentList
+        boardId={boardId}
+        isProcessing={isProcessing}
+        setIsProcessing={setIsProcessing}
+      />
     </Box>
   );
 }
